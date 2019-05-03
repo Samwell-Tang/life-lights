23298ff977891891c4ba739aea3a78505542f9b5
sbhsbhsbh <50141041+sbhsbhsbh@users.noreply.github.com>
2019-05-03 21:55:46 +0800

    基本框架1

---
+++ activitytest/app/src/main/java/com/example/activitytest/secondactivity.java
@@ -0,0 +1,24 @@
+package com.example.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class secondactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondactivity);
+        Button button3 = (Button) findViewById(R.id.button_3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(secondactivity.this,firstactivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
